b2dd4cf2314af818d2aff04b11d5cfc1feb3f6f8
YBRua <ybirua@sjtu.edu.cn>
2023-04-05 15:04:53 +0800

    added more scripts

--- code/gogogo_gogo_csn_eval.sh
+++ code/gogogo_gogo_csn_eval.sh
@@ -1,3 +1,4 @@
+conda activate torch112
 CUDA_VISIBLE_DEVICES=1 python "csn_sampling_bert.py" \
  --msg_len 4 \
  --vocab_source wikitext \
---
+++ code/gogogo_gogo_csn_train_1.sh
@@ -0,0 +1,14 @@
+conda activate torch112
+CUDA_VISIBLE_DEVICES=1 python csn_main_train.py \
+ --msg_len 4 \
+ --data data/CodeSearchNet \
+ --batch_size 80  \
+ --epochs 200 \
+ --save csn_java_noft \
+ --optimizer adam \
+ --use_reconst_loss \
+ --discr_interval 1 \
+ --msg_weight 5 \
+ --gen_weight 1.5 \
+ --reconst_weight 1.5 \
+ --scheduler
