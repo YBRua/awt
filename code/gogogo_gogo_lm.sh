630ef582bea0ebbe5e72ec43ba9c5e9c1ad51f40
YBRua <ybirua@sjtu.edu.cn>
2025-04-08 17:23:01 +0800

    removed unused(?) files

--- code/gogogo_gogo_lm.sh
+++
@@ -1,9 +0,0 @@
-conda activate torch112
-CUDA_VISIBLE_DEVICES=6 python lm_train_cwm.py \
-  --epochs 100 \
-  --data ~/code-watermarking/data/github_c \
-  --lang c \
-  --save github_c_lm.pt \
-  --dropouth 0.2 \
-  --seed 1882 \
-  --bptt 256 \
